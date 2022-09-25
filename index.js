7b0e8ddae35c408ee8c331b36cc72eb7040971a3
+ <+>
2022-09-25 18:58:26 -0400

    commit tree f97130a1aedf88773fa63e71b0d58db3579b6680

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(blocks) { 
+  const result = blocks - 42;
+  return Math.abs(result); 
+} 
+
+function distanceFromHqInFeet(blocks) {
+   const street = distanceFromHqInBlocks(blocks);
+    let result = street * 264;
+
+    return result;
+}
+
+function distanceTravelledInFeet() {
+    
+}
